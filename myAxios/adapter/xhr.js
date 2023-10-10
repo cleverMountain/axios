fb5e62cd3589848c1115e9eb77f290d161b64324
huangjunlin <9579711+huangjunlin511024@user.noreply.gitee.com>
2023-10-11 07:52:13 +0800

    axios

--- myAxios/adapter/xhr.js
+++ myAxios/adapter/xhr.js
@@ -1,16 +1,16 @@
 function xhrAdapter(config) {
   const {url, method} = config
   return new Promise((resolve, reject) => {
-    // let xhr = new XMLHttpRequest()
-    // xhr.onreadystatechange = function () {
-    //   if (xhr.readyState === 4) {
-    //     if ((xhr.status >= 200 && xhr.status < 300)) {
-    //       resolve(1)
-    //     }
-    //   }
-    // }
-    // xhr.open(method, url, false)
-    // xhr.send()
+    let xhr = new XMLHttpRequest()
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4) {
+        if ((xhr.status >= 200 && xhr.status < 300)) {
+          resolve(1)
+        }
+      }
+    }
+    xhr.open(method, url, false)
+    xhr.send()
     const response = {
       data: {a: 1}
     }
--- server/app.js
+++ server/app.js
@@ -1,10 +1,21 @@
 const express = require('express');
-const app= express();
-
-app.get('/user', (req, res)=>{
+const app = express();
+function delayResponse(req, res, next) {
+    const delayTime = 2000; // 延迟时间，以毫秒为单位（这里是2秒）
+    setTimeout(next, delayTime);
+}
+// 使用延迟中间件
+app.use(delayResponse);
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    next();
+});
+app.get('/user', (req, res) => {
     res.send('Hello world');
 });
-app.listen(8089, ()=>{
+app.listen(8089, () => {
     console.log('Server is running at http://127.0.0.1:8089')
 })
 
